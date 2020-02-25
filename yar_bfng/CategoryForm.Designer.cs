fa3eededdfeb735540b42b2e12e114e60a4798f9
unknown <osyaros.2005@yandex.ru>
2020-02-25 16:57:57 +0300

    22.end

--- yar_bfng/CategoryForm.Designer.cs
+++ yar_bfng/CategoryForm.Designer.cs
@@ -29,27 +29,15 @@
         private void InitializeComponent()
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.pictureBox5 = new System.Windows.Forms.PictureBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.pictureBox6 = new System.Windows.Forms.PictureBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.priceTextBox = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.typeComboBox = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -61,125 +49,9 @@
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Visible = false;
             // 
-            // pictureBox2
-            // 
-            this.pictureBox2.Location = new System.Drawing.Point(13, 178);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(179, 120);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 1;
-            this.pictureBox2.TabStop = false;
-            this.pictureBox2.Visible = false;
-            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // textBox2
-            // 
-            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(14, 304);
-            this.textBox2.Multiline = true;
-            this.textBox2.Name = "textBox2";
-            this.textBox2.ReadOnly = true;
-            this.textBox2.Size = new System.Drawing.Size(178, 50);
-            this.textBox2.TabIndex = 2;
-            this.textBox2.Visible = false;
-            // 
-            // textBox6
-            // 
-            this.textBox6.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(816, 304);
-            this.textBox6.Multiline = true;
-            this.textBox6.Name = "textBox6";
-            this.textBox6.ReadOnly = true;
-            this.textBox6.Size = new System.Drawing.Size(178, 50);
-            this.textBox6.TabIndex = 4;
-            this.textBox6.Visible = false;
-            // 
-            // pictureBox3
-            // 
-            this.pictureBox3.Location = new System.Drawing.Point(212, 178);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(179, 120);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
-            this.pictureBox3.Visible = false;
-            this.pictureBox3.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // textBox3
-            // 
-            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(213, 304);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.ReadOnly = true;
-            this.textBox3.Size = new System.Drawing.Size(178, 50);
-            this.textBox3.TabIndex = 6;
-            this.textBox3.Visible = false;
-            // 
-            // pictureBox4
-            // 
-            this.pictureBox4.Location = new System.Drawing.Point(413, 178);
-            this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(179, 120);
-            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox4.TabIndex = 5;
-            this.pictureBox4.TabStop = false;
-            this.pictureBox4.Visible = false;
-            this.pictureBox4.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // textBox4
-            // 
-            this.textBox4.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(414, 304);
-            this.textBox4.Multiline = true;
-            this.textBox4.Name = "textBox4";
-            this.textBox4.ReadOnly = true;
-            this.textBox4.Size = new System.Drawing.Size(178, 50);
-            this.textBox4.TabIndex = 8;
-            this.textBox4.Visible = false;
-            // 
-            // pictureBox5
-            // 
-            this.pictureBox5.Location = new System.Drawing.Point(616, 178);
-            this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(179, 120);
-            this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox5.TabIndex = 7;
-            this.pictureBox5.TabStop = false;
-            this.pictureBox5.Visible = false;
-            this.pictureBox5.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
-            // textBox5
-            // 
-            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(617, 304);
-            this.textBox5.Multiline = true;
-            this.textBox5.Name = "textBox5";
-            this.textBox5.ReadOnly = true;
-            this.textBox5.Size = new System.Drawing.Size(178, 50);
-            this.textBox5.TabIndex = 10;
-            this.textBox5.Visible = false;
-            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
-            // 
-            // pictureBox6
-            // 
-            this.pictureBox6.Location = new System.Drawing.Point(816, 178);
-            this.pictureBox6.Name = "pictureBox6";
-            this.pictureBox6.Size = new System.Drawing.Size(179, 120);
-            this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox6.TabIndex = 9;
-            this.pictureBox6.TabStop = false;
-            this.pictureBox6.Visible = false;
-            this.pictureBox6.Click += new System.EventHandler(this.pictureBox2_Click);
-            // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 369);
+            this.textBox1.Location = new System.Drawing.Point(592, 30);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 11;
@@ -187,7 +59,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(720, 360);
+            this.button1.Location = new System.Drawing.Point(713, 25);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(178, 31);
             this.button1.TabIndex = 12;
@@ -198,82 +70,88 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(21, 31);
+            this.label1.Location = new System.Drawing.Point(25, 36);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(43, 17);
             this.label1.TabIndex = 13;
             this.label1.Text = "Цена";
             // 
-            // textBox7
+            // priceTextBox
             // 
-            this.textBox7.Location = new System.Drawing.Point(74, 25);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(137, 22);
-            this.textBox7.TabIndex = 14;
+            this.priceTextBox.Location = new System.Drawing.Point(78, 30);
+            this.priceTextBox.Name = "priceTextBox";
+            this.priceTextBox.Size = new System.Drawing.Size(137, 22);
+            this.priceTextBox.TabIndex = 14;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(217, 24);
+            this.button2.Location = new System.Drawing.Point(221, 29);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.Size = new System.Drawing.Size(126, 39);
             this.button2.TabIndex = 15;
             this.button2.Text = "Найти";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.filter_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.typeComboBox);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.priceTextBox);
+            this.panel1.Location = new System.Drawing.Point(12, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1044, 95);
+            this.panel1.TabIndex = 16;
+            // 
+            // typeComboBox
+            // 
+            this.typeComboBox.FormattingEnabled = true;
+            this.typeComboBox.Items.AddRange(new object[] {
+            "Процессоры",
+            "Блоки питания",
+            "Системы охолождения",
+            "Видеокарты",
+            "Материнские платы",
+            "Оперативная паамять",
+            "SSD накопители",
+            "HDD накопители"});
+            this.typeComboBox.Location = new System.Drawing.Point(353, 29);
+            this.typeComboBox.Name = "typeComboBox";
+            this.typeComboBox.Size = new System.Drawing.Size(189, 24);
+            this.typeComboBox.TabIndex = 16;
+            this.typeComboBox.SelectedIndexChanged += new System.EventHandler(this.filter_Click);
             // 
             // CategoryForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1012, 450);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.pictureBox6);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.pictureBox5);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.pictureBox4);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.pictureBox2);
+            this.ClientSize = new System.Drawing.Size(1068, 471);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "CategoryForm";
             this.Text = "Категория";
             this.Load += new System.EventHandler(this.CoreForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.PictureBox pictureBox3;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.PictureBox pictureBox5;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.PictureBox pictureBox6;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox priceTextBox;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox typeComboBox;
     }
 }
--- yar_bfng/CategoryForm.cs
+++ yar_bfng/CategoryForm.cs
@@ -15,14 +15,16 @@ namespace yar_bfng
         public string name;
         public string mark;
         public int price;
+        public string type;
         public PictureBox pb;
         public TextBox textbox;
 
-        public Product(string name_, string mark_, int price_)
+        public Product(string name_, string mark_, int price_, string type_)
         {
             name = name_;
             mark = mark_;
             price = price_;
+            type = type_;
             pb = new PictureBox();
             textbox = new TextBox();
         }
@@ -33,7 +35,6 @@ namespace yar_bfng
 
     public partial class CategoryForm : Form
     {
-        Product[] product_list = new Product[100];
 
         void drawPicture(TextBox textBox, PictureBox pictureBox)
         {
@@ -49,25 +50,49 @@ namespace yar_bfng
             }
         }
 
+        Product[] product_list = new Product[21];
         public CategoryForm(string text)
         {
             InitializeComponent();
+            Text = text;
+            pictureBox1.Load("../../Pictures/bg.jpg");
+            BackgroundImage = pictureBox1.Image;
 
-            product_list[0] = new Product("AMD Ryzen 5 2600x", "AMD", 12000);
-            product_list[1] = new Product("AMD Ryzen 7 3700x", "AMD", 22000);
-            product_list[2] = new Product("AMD Ryzen 9", "AMD", 50000);
-            product_list[3] = new Product("Intel Core I9-9900KS", "Intel", 60000);
-            product_list[4] = new Product("Intel i9_9940x", "Intel", 100000);
+            product_list[0] = new Product("AMD Ryzen 5 2600x", "AMD", 12000, "Процессоры");
+            product_list[1] = new Product("AMD Ryzen 7 3700x", "AMD", 22000, "Процессоры");
+            product_list[2] = new Product("AMD Ryzen 9", "AMD", 50000, "Процессоры");
+            product_list[3] = new Product("Intel Core I9-9900KS", "Intel", 60000, "Процессоры");
+            product_list[4] = new Product("Intel i9_9940x", "Intel", 100000, "Процессоры");
+            product_list[5] = new Product("MSI Nvidia Geforce GTX 1660", "MSI", 17000, "Видеокарты");
+            product_list[6] = new Product("ASUS Geforce GTX 1080 Ti", "ASUS", 100000, "Видеокарты");
+            product_list[7] = new Product("MSI Nvidia Geforce RTX 2060", "MSI", 100000, "Видеокарты");
+            product_list[8] = new Product("GIGABYTE nVidia GeForce RTX 2070", "GIGABYTE", 39000, "Видеокарты");
+            product_list[9] = new Product("GIGABYTE Nvidia GeForce RTX 2080", "GIGABYTE", 60000, "Видеокарты");
+            product_list[10] = new Product("AEROCOOL KCAS PLUS 600Вт", "AEROCOOL", 3300, "Блоки питания");
+            product_list[11] = new Product("THERMALTAKE Smart BX1 RGB, 750Вт", "THERMALTAKE", 6000, "Блоки питания");
+            product_list[12] = new Product("be quite DARK POWER PRO 11 850W", "be quite", 9000, "Блоки питания");
+            product_list[13] = new Product("AEROCOOL Strike-X 1100Вт", "AEROCOOL", 10000, "Блоки питания");
+            product_list[14] = new Product("ACCORD GOLD ACC-1500Вт", "TITAN", 10000, "Блоки питания");
+            product_list[15] = new Product("TITAN TTC NK35TZ", "Intel", 5000, "Системы охолождения");
+            product_list[16] = new Product("DEEPCOOL Watercooler GAMMAXX L240T WHITE", "DEEPCOOL", 6000, "Системы охолождения");
+            product_list[17] = new Product("DEEPCOOL Watercooler CASTLE 240 V2", "DEEPCOOL", 4000, "Системы охолождения");
+            product_list[18] = new Product("MSI B450 GAMING PRO CARBON AC", "MSI", 6000, "Материнские платы");
+            product_list[19] = new Product("GIGABYTE B365 M AORUS ELITE", "GIGABYTE", 5000, "Материнские платы");
+            product_list[20] = new Product("GIGABYTE B450 AORUS ELITE", "GIGABYTE", 70000, "Материнские платы");
 
-            int x = 0;
-            for (int i = 0; i < 5; i++)
+
+
+            for (int i = 0; i < product_list.Length; i++)
             {
-                product_list[i].pb.Location = new Point(x, 100);
+                product_list[i].pb.Location = new Point(150 * i, 150);
                 product_list[i].pb.Size = new Size(150, 150);
                 product_list[i].pb.SizeMode = PictureBoxSizeMode.Zoom;
                 product_list[i].pb.Load("../../Pictures/" + product_list[i].name + ".jpg");
+                product_list[i].pb.Click += new EventHandler(pictureBox2_Click);
+                product_list[i].pb.Tag = product_list[i].name;
+                product_list[i].pb.Visible = true;
 
-                product_list[i].textbox.Location = new Point(x, 270);
+                product_list[i].textbox.Location = new Point(150 * i, 250);
                 product_list[i].textbox.Size = new Size(150, 40);
                 product_list[i].textbox.ReadOnly = true;
                 product_list[i].textbox.Enabled = false;
@@ -76,70 +101,18 @@ namespace yar_bfng
 
                 Controls.Add(product_list[i].pb);
                 Controls.Add(product_list[i].textbox);
-                x = x + 200;
             }
 
 
-            Text = text;
             button1.Visible = false;
-            pictureBox1.Load("../../Pictures/bg.jpg");
-            BackgroundImage = pictureBox1.Image;
-            TextBox[] tb = new TextBox[20];
-
-            tb[0] = textBox2;
-            tb[1] = textBox3;
-            tb[2] = textBox4;
-            tb[3] = textBox5;
-            tb[4] = textBox6;
-
-            textBox2.Text = "";
-
-            if (text == "Процессоры")
-            {
-                textBox2.Text = "AMD Ryzen 5 2600x";
-                textBox3.Text = "AMD Ryzen 7 3700x";
-                textBox4.Text = "AMD Ryzen 9";
-                textBox5.Text = "Intel Core I9-9900KS" +
-                    "";
-                textBox6.Text = "Intel i9_9940x";
-            }
-            if (text == "Видеокарты")
-            {
-                textBox2.Text = "MSI Nvidia Geforce GTX 1660";
-                textBox3.Text = "ASUS Geforce GTX 1080 Ti";
-                textBox4.Text = "MSI Nvidia Geforce RTX 2060";
-                textBox5.Text = "GIGABYTE nVidia GeForce RTX 2070";
-                textBox6.Text = "GIGABYTE Nvidia GeForce RTX 2080";
-            }
-            if (text == "Блоки питания")
-            {
-                textBox2.Text = "AEROCOOL KCAS PLUS 600Вт";
-                textBox3.Text = "THERMALTAKE Smart BX1 RGB, 750Вт";
-                textBox4.Text = "be quite DARK POWER PRO 11 850W";
-               /* for (int i = 3; i < 5; i++)
-                {
-                    tb[i].Visible = false;
-                }*/
-                textBox5.Text = "AEROCOOL Strike-X 1100Вт";
-                textBox6.Text = "ACCORD GOLD ACC-1500Вт";
-            }
 
             if (text == "Системы охолождения")
             {
                 button1.Visible = true;
-                textBox2.Text = "TITAN TTC NK35TZ";
-                textBox3.Text = "DEEPCOOL Watercooler GAMMAXX L240T WHITE";
-                textBox4.Text = "DEEPCOOL Watercooler CASTLE 240 V2";
                 button1.Tag = "http://hyperpc.ru";
-
-
             }
 
-            drawPicture(textBox2, pictureBox2);
-            drawPicture(textBox3, pictureBox3); 
-            drawPicture(textBox4, pictureBox4);
-            drawPicture(textBox5, pictureBox5);
-            drawPicture(textBox6, pictureBox6);   
+            filter_Click(null, null);
         }
 
         private void CoreForm_Load(object sender, EventArgs e)
@@ -167,30 +140,49 @@ namespace yar_bfng
             }
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void filter_Click(object sender, EventArgs e)
         {
             int x = 0;
             int y = 100;
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < product_list.Length; i++)
             {
-                product_list[i].pb.Visible = false;
-                product_list[i].textbox.Visible = false;
+                product_list[i].pb.Visible = true;
+                product_list[i].textbox.Visible = true;
 
-                if (product_list[i].price <= Convert.ToInt32(textBox7.Text))
+                if (priceTextBox.Text != "" &&
+                    product_list[i].price >= Convert.ToInt32(priceTextBox.Text))
                 {
-                    product_list[i].pb.Visible = true;
-                    product_list[i].textbox.Visible = true;
+                    product_list[i].pb.Visible = false;
+                    product_list[i].textbox.Visible = false;
+                }
+                else if (typeComboBox.Text != "" &&
+                    product_list[i].type != typeComboBox.Text)
+                {
+                    product_list[i].pb.Visible = false;
+                    product_list[i].textbox.Visible = false;
+                }
+
+
+                if (product_list[i].pb.Visible)
+                {
                     product_list[i].pb.Location = new Point(x, y);
                     product_list[i].textbox.Location = new Point(x, y + 170);
                     x = x + 200;
 
                     if (x > Width - 200)
                     {
-                        y = y  + 250;
+                        y = y + 250;
                         x = 0;
                     }
                 }
             }
+        }
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            filter_Click(null, null);
         }
     }
 }
